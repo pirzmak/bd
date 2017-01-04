918076a415f6e40ca4793446aacc3edfb8d299a7
xarines <wmarch112@gmail.com>
2017-01-04 18:44:20 +0100

    first commit

---
+++ src/main/java/pl/edu/pw/elka/BdApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.elka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdApplication.class, args);
+	}
+}
